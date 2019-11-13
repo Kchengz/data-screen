3891493057404c6abef4d095026364dc13abcfc3
kcz <1150791676@qqcom>
2019-11-13 15:01:08 +0800

    初始化源码

---
+++ src/components/datav/CenterCmp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="center-cmp">
+<dv-flyline-chart :config="config" style="width:100%;height:100%;" />
+  </div>
+</template>
+
+<script>
+// import LabelTag from './LabelTag'
+
+export default {
+  name: 'CenterCmp',
+  data () {
+    return {
+      config: {
+        centerPoint: [0.56, 0.68],
+        points: [
+          [0.52, 0.23],
+          [0.43, 0.29],
+          [0.59, 0.35],
+          [0.53, 0.47],
+          [0.45, 0.54],
+          [0.36, 0.38],
+          [0.62, 0.55],
+          [0.56, 0.56],
+          [0.37, 0.66],
+          [0.55, 0.81],
+          [0.55, 0.67],
+          [0.37, 0.29],
+          [0.20, 0.36],
+          [0.76, 0.41],
+          [0.59, 0.18],
+          [0.68, 0.17],
+          [0.59, 0.10]
+        ],
+        bgImgUrl: 'http://datav.jiaminghi.com/img/flylineChart/map.jpg'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.center-cmp {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+
+}
+</style>
